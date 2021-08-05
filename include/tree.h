1b0347d8d6ff44a78e5fbd24cdd2a0439a46b3de
Magdalena Paszczuk <magdapaszczuk@gmail.com>
2021-08-05 19:28:55 +0200

    Add rb_insert

--- include/tree.h
+++ include/tree.h
@@ -60,3 +60,69 @@ void rb_right_rotate(tree_t *tree, node_t *y) {
     x->right = y;
     y->parent = x;
 }
+
+void rb_insert(tree_t *tree, node_t *z) {
+    node_t *y = tree->nil;
+    node_t *x = tree->root;
+    while (x != tree->nil) {
+        y = x;
+        if (z->key < x->key) {
+            x = x->left;
+        } else {
+            x = x->right;
+        }
+    }
+    z->parent = y;
+    if (y == tree->nil) {
+        tree->root = z;
+    } else if (z->key < y->key) {
+        y->left = z;
+    } else {
+        y->right = z;
+    }
+    z->left = tree->nil;
+    z->right = tree->nil;
+    z->color = RED;
+}
+
+void rb_insert_fixup(tree_t *tree, node_t *z){
+    while (z->parent->color == RED){
+        if(z->parent == z->parent->parent->left){
+            node_t *y = z->parent->parent->right;
+            if(y->color == RED){
+                z->parent->color = BLACK;
+                y->color = BLACK;
+                z->parent->parent->color = RED;
+                z = z->parent->parent;
+            }
+            else if(z == z->parent->right){
+                z = z->parent;
+                rb_left_rotate(tree, z);
+            }
+            else{
+                z->parent->color = BLACK;
+                z->parent->parent->color = RED;
+                rb_right_rotate(tree, z->parent->parent);
+            }
+        }
+        else{
+            //TODO to jest zjebane
+            node_t *y = z->parent->parent->left;
+            if(y->color == RED){
+                z->parent->color = BLACK;
+                y->color = BLACK;
+                z->parent->parent->color = RED;
+                z = z->parent->parent;
+            }
+            else if(z == z->parent->left){
+                z = z->parent;
+                rb_left_rotate(tree, z);
+            }
+            else{
+                z->parent->color = BLACK;
+                z->parent->parent->color = RED;
+                rb_right_rotate(tree, z->parent->parent);
+            }
+        }
+    }
+}
